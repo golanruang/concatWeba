ef9eec05b5f0b60974045e935eb1711d1f813ae6
golanruang <guang.rolan@gmail.com>
2020-07-10 15:25:50 -0700

    Working audio concatenator + some sample files
    added sample files

--- a2.py
+++
@@ -1,7 +0,0 @@
-import glob
-import os
-
-stringa = ""
-for f in glob.glob("*.weba"):
-    stringa += f + "|"
-os.system("ffmpeg -i \"concat:" + stringa + "\" -codec copy output.wav")
--- audioThing.py
+++ audioThing.py
@@ -2,38 +2,43 @@
 This script uses ffmpeg to merge all weba files in the
 folder which the script is run. You need to have ffmpeg
 installed on your computer.
+To install, run this:
+brew install ffmpeg-python
+
 """
 import glob
 import ffmpeg
 import os
 from os.path import isdir, expanduser
 
-def concatVideos(files):
+def concatVideos(files,path):
+    """
+    Purpose: concatenate the weba files into a mp4 file (downloads the file on computer)
+    Parameters: a list of files to concatenate
+    Return val: None
+    """
     name = findID(files[0]) + '.mp4'
-    print('name: '+name)
+    pathName = path+name
     streams = []
     for file in files:
         streams.append(ffmpeg.input(file))
-    print(streams)
-    ffmpeg.concat(*streams,v=0,a=1).output('pizza.mp4').run()
-
-# def findWeba(path, pattern):
-#     files = []                      # all weba filenames
-#     print('all files: ', os.listdir(path).sort())
-#     for file in os.listdir(path):
-#         if pattern in file:
-#             files.append(file)
-#     return files
+    ffmpeg.concat(*streams,v=0,a=1).output(pathName).run()
 
 def findWeba(path):
-    #home = expanduser("~")
-    #path = home +'/Documents/programming stuff/audioStuff/uploads'
-    # all weba filenames
+    """
+    Purpose: finds all weba files in a given directory
+    Parameters: the path of the directory with the weba files in it (str)
+    Return: a list with all the weba files names in it (list of strings)
+    """
     weba_files = [f for f in os.listdir(path) if f.endswith('.weba')]
-    #print(text_files)
     return weba_files
 
 def findID(file):
+    """
+    Purpose: find the id of file (str)
+    Parameters: the file name (str)
+    Return: the id number of the file (str)
+    """
     numDashes=0
     numNeededDashes=4
     ID = ''
@@ -45,74 +50,31 @@ def findID(file):
                     ID = ID + file[char+i]
     return ID
 
-def stripFilename(file):
-    newName = ''
-    for char in file:
-        if char == '.':
-            return newName
-        newName = newName + char
-
 def main():
     home = expanduser("~")
-    path = home +'/Documents/programming stuff/audioStuff/works'
-    files = findWeba(path)
+    path = input('Enter path: ')            # input for path that audio files/this file are in
+    path = home + path
+    files = findWeba(path)                  # finds all the weba files in the file
     sortedFiles = sorted(files)
-    # for file in sortedFiles:
-    #     print(file)
     idFiles = []
     index = 0
     BOOL = True
-    while sortedFiles!=[]:
+    while sortedFiles!=[]:                  # while not interated through all weba files
         if idFiles == []:
+            idFiles.append(sortedFiles[0])  # grouping the files by id number
+            sortedFiles.pop(0)
+        if len(sortedFiles) == 1:
             idFiles.append(sortedFiles[0])
             sortedFiles.pop(0)
-        print('idFiles ID: ' + str(findID(idFiles[0])) + 'sortedFilesID: ' + str(findID(sortedFiles[0])))
+            concatVideos(idFiles,path)
+            idFiles=[]
+            break
         if findID(idFiles[0])==findID(sortedFiles[0]):
             idFiles.append(sortedFiles[0])
             sortedFiles.pop(0)
         if findID(idFiles[0])!=findID(sortedFiles[0]):
             print(idFiles)
-            concatVideos(idFiles)
+            concatVideos(idFiles,path)
             idFiles=[]
-        #
-        #
-        #
-        #
-        #
-        #
-        #
-        # id=findID(files[0])
-        #
-        #
-        #
-        #
-        #
-        #
-        #
-        # print(len(files))
-        # print(range(len(files)))
-        # for i in range(len(files)):
-        #     #print('OG ID: ' + str(id) + ' ID: ' + str(findID(files[i])) + ' BOOL: ' + str(findID(files[i]) == id))
-        #     print(i)
-        #     if findID(files[i]) == id:
-        #         idFiles.append(files[i])
-        #         files.pop(i)
-        # print(idFiles)
-        # concatVideos(idFiles, path)
-        #
-        # # print(index)
-        # # id = findID(files[index])
-        # # print("file #" + str(index) + "is: " + str(files[index]))
-        # # if len(idFiles)==0:
-        # #     idFiles.append(files[index])
-        # # elif id == findID(idFiles[0]):
-        # #     idFiles.append(files[index])
-        # # #if id != findID(idFiles[0]):
-        # # else:
-        # #     print("idFiles: ",idFiles)
-        # #     concatVideos(idFiles, path)
-        # #     idFiles = []
-        # #     idFiles.append(id)
-        # # index+=1
 
 main()
